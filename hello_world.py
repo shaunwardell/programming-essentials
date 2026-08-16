2f04f39b8f23d912be9de29053a45f5f76acb1b5
shaunwardell <s.epnett.efg26@outlook.com>
2026-08-16 11:06:57 -0500

    Refactor hello_world.py with formatted strings

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,18 @@
-print('Hello World!')
-print('My name is Shaun.')
-print('I am learning Python at DeVry University.')
-print('Welcome to my programming portfolio!')
+# My first Python program
+# Programming Essentials
+
+name = "Shaun"
+school = "DeVry University"
+course = "Programming Essentials"
+age = 49
+
+print("Hello World")
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I study {course} at {school}.")
+
+current_age = int(input("How old are you? "))
+future_age = current_age + 10
+
+print(f"In 10 years, you will be {future_age} years old.")
